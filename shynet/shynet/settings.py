3ebe4b21e2cf58eb54e452d955fedadea048aa02
starkzarn <starkzarn@zarn.me>
2024-06-03 13:51:09 -0700

    migrate psycopg2 to psycopg3

--- shynet/shynet/settings.py
+++ shynet/shynet/settings.py
@@ -117,7 +117,7 @@ if os.getenv("SQLITE", "False") == "True":
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "ENGINE": "django.db.backends.postgresql",
             "NAME": os.environ.get("DB_NAME"),
             "USER": os.environ.get("DB_USER"),
             "PASSWORD": os.environ.get("DB_PASSWORD"),
