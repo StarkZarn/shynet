36850eeeff96d7c39f6b35061c690637dd425356
starkzarn <starkzarn@zarn.me>
2024-06-26 10:08:31 -0700

    add django-axes for auth protection

--- shynet/shynet/settings.py
+++ shynet/shynet/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "debug_toolbar",
     "corsheaders",
+    "axes",
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+    # AxesMiddleware should be the last middleware in the MIDDLEWARE list.
+    # It only formats user lockout messages and renders Axes lockout responses
+    # on failed user authentication attempts from login views.
+    # If you do not want Axes to override the authentication response
+    # you can skip installing the middleware and use your own views.
+    "axes.middleware.AxesMiddleware",
 ]
 
 ROOT_URLCONF = "shynet.urls"
@@ -254,6 +261,8 @@ if not DEBUG and os.getenv("REDIS_CACHE_LOCATION") is not None:
 AUTH_USER_MODEL = "core.User"
 
 AUTHENTICATION_BACKENDS = (
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    "axes.backends.AxesStandaloneBackend",
     "rules.permissions.ObjectPermissionBackend",
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
