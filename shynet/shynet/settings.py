5693a0015862aebb6ba0c511c69237829fb56e50
StarkZarn <starkzarn@zarn.me>
2025-11-13 16:07:28 -0800

    fix env var fetch

--- shynet/shynet/settings.py
+++ shynet/shynet/settings.py
@@ -43,7 +43,7 @@ CSRF_TRUSTED_ORIGINS = filter(
     lambda k: len(k) > 0, os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 )
 
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "[*]")
+ os.getenv("CORS_ALLOWED_ORIGINS", "[*]")
 
 # Application definition
 
